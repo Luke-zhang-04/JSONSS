24a383308dce8018122bc0870a7971957d0bb355
Luke <55749227+Luke-zhang-04@users.noreply.github.com>
2020-05-10 18:04:01 -0400

    added shallow formatting

---
+++ parser/parser.ts
@@ -0,0 +1,38 @@
+/*
+ *  JSONSS; JavaScript Object Cascading Style Sheets
+ *  Copyright (C) 2020 Luke Zhang
+ *
+ *  https://luke-zhang-04.github.io
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+function parseJsonss(styles: object, pretty: boolean): string {
+    let output: string = ""
+    for (const [key, value] of Object.entries(styles)) {
+        let val
+        if (pretty) {
+            val = JSON.stringify(value).replace("_", "-").replace(",", ";\n  ").replace(/"/g, "").replace(/{/g, "").replace(/}/g, "")
+            output += `${key.replace("_", "-")}{\n  ${val}\n}\n`
+        } else {
+            val = JSON.stringify(value).replace("_", "-").replace(",", ";").replace(/"/g, "")
+            output += `${key.replace("_", "-")} ${val}`
+        }
+    }
+    return output
+}
+
+module.exports = {
+    parser: parseJsonss
+}
